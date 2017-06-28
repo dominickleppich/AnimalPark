b54dfb026df4c0057da0feb87d6dfe4373cdd828
Dominick Leppich <dominick.leppich@gmail.com>
2017-06-29 01:41:36 +0200

    [+] Boot class

---
+++ source/eu/nepster/animalpark/Boot.java
@@ -0,0 +1,10 @@
+package eu.nepster.animalpark;
+
+/**
+ * @author Dominick Leppich
+ */
+public class Boot {
+    public static void main(String[] args) {
+        System.out.println("Laeuft!!!");
+    }
+}
