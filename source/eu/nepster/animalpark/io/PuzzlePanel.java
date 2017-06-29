a1164cc232e59ae3d3870dac4322dee101c597e3
Dominick Leppich <dominick.leppich@gmail.com>
2017-06-29 04:08:31 +0200

    [~] Working PuzzlePanel

--- source/eu/nepster/animalpark/io/PuzzlePanel.java
+++ source/eu/nepster/animalpark/io/PuzzlePanel.java
@@ -6,19 +6,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.Ellipse2D;
 
 /**
  * Created by dominick on 6/29/17.
  */
 public class PuzzlePanel extends JPanel implements MouseListener {
     private static final int FIELD_SIZE = 50;
+    private static final int STROKE = 2;
     private static final Color[] COLORS = new Color[]{Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.GRAY, Color.PINK, Color.ORANGE};
 
     private Puzzle puzzle;
 
     public PuzzlePanel(Puzzle puzzle) {
         this.puzzle = puzzle;
-        setPreferredSize(new Dimension(FIELD_SIZE * puzzle.getSize(), FIELD_SIZE * puzzle.getSize()));
+        setPreferredSize(new Dimension(FIELD_SIZE * puzzle.getSize() + STROKE, FIELD_SIZE * puzzle.getSize() + STROKE));
 
         addMouseListener(this);
     }
@@ -32,7 +34,7 @@ public class PuzzlePanel extends JPanel implements MouseListener {
 
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        g.setStroke(new BasicStroke(1f));
+        g.setStroke(new BasicStroke(STROKE));
 
         int mark = 0;
 
@@ -41,7 +43,10 @@ public class PuzzlePanel extends JPanel implements MouseListener {
                 g.setColor(COLORS[puzzle.getColor(x, y)]);
                 g.fillRect(x * FIELD_SIZE, y * FIELD_SIZE, FIELD_SIZE, FIELD_SIZE);
                 // TODO Draw mark
-
+                if (puzzle.isMark(x, y)) {
+                    g.setColor(Color.WHITE);
+                    g.fill(new Ellipse2D.Float(x * FIELD_SIZE + 10, y * FIELD_SIZE + 10, FIELD_SIZE - 20, FIELD_SIZE - 20));
+                }
 
                 g.setColor(Color.BLACK);
                 g.drawRect(x * FIELD_SIZE, y * FIELD_SIZE, FIELD_SIZE, FIELD_SIZE);
@@ -56,9 +61,9 @@ public class PuzzlePanel extends JPanel implements MouseListener {
 
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
-        System.out.println("Clicked " + mouseEvent.getX() + ", " + mouseEvent.getY());
-
-        puzzle.setMark(mouseEvent.getX() / FIELD_SIZE, mouseEvent.getY() / FIELD_SIZE, true);
+        int x = mouseEvent.getX() / FIELD_SIZE;
+        int y = mouseEvent.getY() / FIELD_SIZE;
+        puzzle.setMark(x, y, !puzzle.isMark(x, y));
     }
 
     @Override
