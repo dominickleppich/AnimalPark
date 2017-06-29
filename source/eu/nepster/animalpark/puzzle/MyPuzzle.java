7c5941baad19a2eaf97d398a136dea924df18231
knuddelsloth <nicorehe@gmx.net>
2017-06-29 04:05:53 +0200

    [+] My Puzzle done

--- source/eu/nepster/animalpark/puzzle/MyPuzzle.java
+++ source/eu/nepster/animalpark/puzzle/MyPuzzle.java
@@ -2,16 +2,18 @@ package eu.nepster.animalpark.puzzle;
 
 import java.util.Observable;
 
+import static java.lang.Math.signum;
 import static java.lang.Math.sqrt;
 
 public class MyPuzzle extends Puzzle{
     private Field [][] fields;
-    private int[] color;
     private int size;
+    private int parks;
 
-    public MyPuzzle(int[] board, int parks){
+    public MyPuzzle(int[] board, int p){
+        parks = p;
         size = (int) sqrt(board.length);
-        color = new int[size];
+        fields = new Field[size][size];
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
                 fields[i][j].setColor(board[(i*size) + j]);
@@ -22,6 +24,8 @@ public class MyPuzzle extends Puzzle{
     @Override
     public void setMark(int x, int y, boolean mark){
         fields[x][y].setMark(mark);
+        setChanged();
+        notifyObservers(isWin());
     }
 
     @Override
@@ -37,5 +41,42 @@ public class MyPuzzle extends Puzzle{
     @Override
     public int getSize(){
         return size;
+    }
+
+    private boolean isWin(){
+        int[] color = new int[size];
+        int[] cols = new int[size];
+        int[] rows = new int[size];
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j< size; j++){
+                if(!fields[i][j].isMark())
+                    continue;
+                if(checkNeighbor(i,j))
+                    return false;
+                cols[i]++;
+                rows[j]++;
+                color[fields[i][j].getColor()]++;
+            }
+        }
+        for(int i = 0; i < size; i++){
+            if(cols[i] != parks || rows[i] != parks || color[i] != parks)
+                return false;
+        }
+        return true;
+    }
+
+    private boolean checkNeighbor(int x, int y){
+        for( int i = -1; i <= 1; ++i ) {
+            for( int j = -1; j <= 1; ++j ) {
+                if(x == 0 && y == 0)
+                    continue;
+                if( i + x >= 0 && i + x < size &&
+                        j + y >= 0 && j + y < size) {
+                    if(fields[i+x][j+y].isMark())
+                        return true;
+                }
+            }
+        }
+        return false;
     }
 }
